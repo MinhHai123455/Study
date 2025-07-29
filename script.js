9618b17ab92492ee905fddde9a53d3c129905383
MinhHai123455 <haip27378@gmail.com>
2025-07-29 19:58:00 +0700

    sua file css

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const grid = document.querySelector('.grid');
-for (let i = 0; i < 200; i++) {
+const grid = document.getElementById('grid');
+
+// Tạo ô lưới 10 x 20
+for (let i = 0; i < 64; i++) {
   const cell = document.createElement('div');
-  cell.classList.add('cell');
   grid.appendChild(cell);
 }
-function startGame(){
-    alert("Game Started!");
+
+function startGame() {
+  alert("Chức năng đang phát triển...");
 }
